3b95f8902850944b8755e9509768f0c4d951042f
Kevin <kevinmoore9@users.noreply.github.com>
2017-01-31 22:06:22 -0800

    create users table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# These are extensions that must be enabled in order to support this database
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170130231941) do
 
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
 
